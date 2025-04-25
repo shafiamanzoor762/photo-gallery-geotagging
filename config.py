0acaec7f5b93d6df85c7ae36e7ea4d2f7dac3893
AIMEN-10 <aimen5995@gmail.com>
2025-04-25 15:51:27 +0500

    Aimen

--- config.py
+++ config.py
@@ -10,14 +10,14 @@ app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024
 # Shafia (Macbook)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://sa:123@WINDOWS-R5NK4VK\SQLEXPRESS/PhotoGallery?driver=ODBC+Driver+17+for+SQL+Server'
 # Shafia (HP laptop)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://DESKTOP-831NKE6/PhotoGallery?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes&autocommit=True'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://DESKTOP-831NKE6/PhotoGallery?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes&autocommit=True'
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://sa:1234@DESKTOP-L6C900S\\SQLEXPRESS/PhotoGalleryFYP?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes&autocommit=True'
 # Configure your database URI
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://sa:12345@DESKTOP-4S96KP6\SQLEXPRESS/PhotoGallery1?driver=ODBC+Driver+17+for+SQL+Server'
 
 #aimen db connection string 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://sa:123@DESKTOP-R50Q2I1\\SQLEXPRESS/FYP2PhotoGallery?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes&autocommit=True'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://sa:123@DESKTOP-R50Q2I1\\SQLEXPRESS/FYP2PhotoGallery?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes&autocommit=True'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
